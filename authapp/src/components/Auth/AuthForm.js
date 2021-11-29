d53b8e0d463662d48db1f1540790feb8b9bccbda
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-29 11:55:11 -0500

    Using refs to get the entered input data

--- authapp/src/components/Auth/AuthForm.js
+++ authapp/src/components/Auth/AuthForm.js
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import './AuthForm.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const emailRef = useRef()
+  const passwordRef = useRef()
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
 
   return (
     <section className='auth'>
@@ -15,11 +22,11 @@ const AuthForm = () => {
       <form>
         <div className='control'>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailRef} />
         </div>
         <div className='control'>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordRef} />
         </div>
         <div className='actions'>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
