3f6710efecf2321594d8fc692de325c2891c28e3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-30 11:30:48 -0500

    sent a post request to the url which allows to change password and added some error handling

--- authapp/src/components/profileform/ProfileForm.js
+++ authapp/src/components/profileform/ProfileForm.js
@@ -1,11 +1,54 @@
+import { useRef, useContext, useState } from 'react'
 import './ProfileForm.css';
+import AuthContext from '../../store/auth-context';
+
+const KEY = process.env.REACT_APP_API_KEY
+
+
 
 const ProfileForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const newPasswordRef = useRef()
+
+  const context = useContext(AuthContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const enteredNewPassword = newPasswordRef.current.value
+    setLoading(true)
+
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: context.token,
+        password: enteredNewPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      setLoading(false)
+      if (res.ok) {
+        return res.json()
+      } else {
+        return res.json().then(data => {
+          let errorMessage = "Authentication failed!"
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message
+          }
+          throw new Error(errorMessage)
+        })
+      }
+    })
+  }
   return (
-    <form className='forms'>
+    <form className='forms' onSubmit={handleSubmit}>
       <div className='controls'>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordRef} />
       </div>
       <div className='actions'>
         <button>Change Password</button>
