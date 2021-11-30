16f51db51020a4b31c3cdd64c2cc387b840a0ef3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-30 17:02:55 -0500

    If user tries to go to /profile and is not logged in they will be redirected to the login form

--- authapp/src/App.js
+++ authapp/src/App.js
@@ -1,9 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import UserProfile from './components/userprofile/UserProfile';
 import AuthPage from './components/authpage/AuthPage';
 import HomePage from './components/homepage/HomePage';
-
 import { useContext } from 'react'
 import AuthContext from './store/auth-context';
 
@@ -14,8 +13,10 @@ function App() {
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/auth' element={<AuthPage />} />
+        {!context.isLoggedIn && <Route path='/auth' element={<AuthPage />} />}
         {context.isLoggedIn && <Route path='/profile' element={<UserProfile />} />}
+        {!context.isLoggedIn && <Route path='/profile' element={<Navigate replace to='/auth' />} />}
+        <Route path='*' element={<Navigate replace to='/' />} />
       </Routes>
     </Layout>
   );
